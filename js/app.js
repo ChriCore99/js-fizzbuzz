5f3c20467b3789f06c889be85bc07d4ff6821cea
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-06 15:05:12 +0100

    ADD: struttura di base e primo loop

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// dichiaro una variabile di 100
+
+let numero = 100; 
+
+// apro un cliclo pe rstampare i numeri da 0 a 99
+
+for (let i = 0; i < 100; i++) {
+    let nimero = i + 1;
+    console.log(numero);
+}
