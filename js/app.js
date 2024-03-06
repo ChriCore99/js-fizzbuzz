9a09413d899841fb17b3b442a74cdc7c47a62f96
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-06 18:29:44 +0100

    ADD: valori numeri griglia

--- js/app.js
+++ js/app.js
@@ -11,23 +11,17 @@ for (let i = 0; i < n; i++) {
     // creo elemento "list item" valido per ogni numero della lista
     const liDOMElement = document.createElement('li');
 
-    // dico a ogni list item di scrivere in html un numero
-    liDOMElement.innerHTML = numero;
-
-    // assegno ai list items la classe di prova
-    liDOMElement.classList.add('prova');
-
-    // dico alla unordered list di scrivere ogni list item creato, uno sotto l'altro, senza cambiarne i valori
-    ulDOMElement.append(liDOMElement);
-
     // creo variabile "se un numero è multiplo di 3"
-    let multiplo3 = numero % 3; 
+    let multiplo3 = numero % 3;
 
     // creo variabile "se un numero è multiplo di 5"
     let multiplo5 = numero % 5; 
 
+    // creo variabile "se multiplo di 3 e di 5"
+    let multiplo15 = (multiplo3 === 0) && (multiplo5 === 0);   // (multiplo di 3) + (multiplo di 5)
+
     // stampa dei numeri
-    if ((multiplo3 === 0) && (multiplo5 === 0)) {   // (multiplo di 3) + (multiplo di 5)
+    if (multiplo15) {
         console.log('FizzBuzz');
     }
 
@@ -42,5 +36,14 @@ for (let i = 0; i < n; i++) {
     else {
         console.log(numero);
     }
+
+        // dico a ogni list item di scrivere in html un numero
+        liDOMElement.innerHTML = multiplo15 || multiplo3 || multiplo5 || numero;
+
+        // assegno ai list items la classe di prova
+        liDOMElement.classList.add('prova');
+    
+        // dico alla unordered list di scrivere ogni list item creato, uno sotto l'altro, senza cambiarne i valori
+        ulDOMElement.append(liDOMElement);
 
 }
