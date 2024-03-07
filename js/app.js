142dae64d003de627da4f57b50ca956060a108b0
ChriCore99 <christian.corelli.io@gmail.com>
2024-03-07 10:41:23 +0100

    ADD: correzione codice post lezione

--- js/app.js
+++ js/app.js
@@ -15,33 +15,28 @@ for (let i = 0; i < n; i++) {
     let multiplo3 = numero % 3;
 
     // creo variabile "se un numero è multiplo di 5"
-    let multiplo5 = numero % 5; 
-
-    // creo variabile "se multiplo di 3 e di 5"
-    let multiplo15 = (multiplo3 === 0) && (multiplo5 === 0);   // (multiplo di 3) + (multiplo di 5)
+    let multiplo5 = numero % 5;  
 
     // stampa dei numeri
-    if (multiplo15) {
+    if ((multiplo3 === 0) && (multiplo5 === 0)) {
         console.log('FizzBuzz');
+        liDOMElement.innerHTML += `<div> FizzBuzz </div>`
     }
 
     else if (multiplo3 === 0) {
         console.log('Fizz');
+        liDOMElement.innerHTML += `<div> Fizz </div>`
     }
 
     else if (multiplo5 === 0) {
         console.log('Buzz');
+        liDOMElement.innerHTML += `<div> Buzz </div>`
     }
 
     else {
         console.log(numero);
+        liDOMElement.innerHTML += `<div> ${numero} </div>`
     }
-
-        // dico a ogni list item di scrivere in html un numero
-        liDOMElement.innerHTML = multiplo15 || multiplo3 || multiplo5 || numero;
-
-        // assegno ai list items la classe di prova
-        liDOMElement.classList.add('prova');
     
         // dico alla unordered list di scrivere ogni list item creato, uno sotto l'altro, senza cambiarne i valori
         ulDOMElement.append(liDOMElement);
